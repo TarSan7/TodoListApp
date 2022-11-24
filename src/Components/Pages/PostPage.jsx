fc359a4a97867892dda2c8da4f4afa2a30fc19e8
TarSan7 <kvbrd110820162@gmail.com>
2022-11-24 12:43:37 +0200

    Caching data

--- src/Components/Pages/PostPage.jsx
+++ src/Components/Pages/PostPage.jsx
@@ -1,18 +1,28 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import {PostContext} from "../../Context/PostContext";
+import { useLocation } from 'react-router-dom';
 
-function PostPage(props) {
-    const posts = useContext(PostContext);
+function PostPage() {
+    // const posts = queryClient.getQueryData('posts');
     const params = useParams();
-
-    useEffect(() => {
-        console.log(posts);
-    }, [])
+    const { state } = useLocation();
 
     return (
-        <div className="list-of-posts">
-
+        <div>
+            <h1>{ state.title }</h1>
+            <div className="list-of-posts">
+                <div className="post-content">
+                    <img src="https://picsum.photos/200/300" className="image"/>
+                    <div className="full-width">
+                        <span className="post-text"> { state.selftext } </span>
+                        <div className="post-content">
+                            <div className="post-text"> { state.subreddit } </div>
+                            <div className="post-text">Author: { state.author } </div>
+                            <div className="post-text">Reddit <a href={ state.url }>Link</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
